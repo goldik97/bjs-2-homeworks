0e59471f2cd9ad4af372611bb2702b41d138d05b
Roman Goldenshteyn <s-221@mail.ru>
2024-06-15 11:41:19 +0300

    готовое 4е задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+   if(this.hasOwnProperty('marks') === true) {
+    this.marks.push(...marks);
+   }
 }
 
 Student.prototype.getAverage = function () {
-  
-}
+      if(this.hasOwnProperty('marks') === false || this.marks.length === 0) {
+        return 0;
+      }
+
+      return this.marks.reduce((acc, item, index) => {
+        acc += item;
+
+      if(index === this.marks.length - 1) {
+        return acc / this.marks.length;
+      }
+      return acc}, 0);
+    }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
